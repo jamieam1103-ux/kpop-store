d775a14f4ee13d5b873b10935776874ef1f8bab7
Jay <jamieam1103@gmail.com>
2026-05-20 17:25:43 -0500

    setup inicial del proyecto

---
+++ src/main/java/com/web/kpop_store/KpopStoreApplication.java
@@ -0,0 +1,13 @@
+package com.web.kpop_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpopStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpopStoreApplication.class, args);
+	}
+
+}
